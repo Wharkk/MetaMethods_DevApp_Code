b12a856bcddb9c82b11ad08fe06a9e4d75c52726
Wharkk <76532812+Wharkk@users.noreply.github.com>
2025-04-19 10:14:58 +0200

    Create DevDataTemplate.lua

---
+++ DatastoreUtils/Templates/DevDataTemplate.lua
@@ -0,0 +1,17 @@
+local accounts = {
+	Savings = {
+		Balance = 0,
+		Transactions = {}
+	},
+	
+	Spending = {
+		Balance = 0,
+		Transactions = {}
+	}
+}
+
+local template = {
+	Accounts = accounts
+}
+
+return template
